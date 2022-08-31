1db34d3d647cb5768ac53264cba06701f499924d
Henrique <henrique5044@gmail.com>
2022-08-31 17:19:02 -0300

    Exercise4: List<Double> coins modificado para List<Integer> coins.

--- src/ExercisesNormal/Exercise4.java
+++ src/ExercisesNormal/Exercise4.java
@@ -1,7 +1,8 @@
 package ExercisesNormal;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Arrays;
-import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -14,49 +15,51 @@ public class Exercise4 {
         double wallet = scan.nextDouble();
         try {
             calculateChange(wallet);
-        } catch (ArithmeticException e) {
-            System.out.println("Dinheiro insuficiente");
         } catch (IllegalArgumentException e) {
             System.out.println("Valores acima de R$100 são proibidos");
-        } catch (InputMismatchException e) {
-            System.out.println("Não há troco para receber");
         }
         System.out.println(calculateChange(wallet));
     }
 
     public static String calculateChange(double money) {
         List<Integer> notes = Arrays.asList(50, 20, 10, 5, 2);
-        List<Double> coins = Arrays.asList(1.0, 0.5, 0.25, 0.10, 0.5, 0.1);
-        double toyPrice = 8.5;
-        double change, changeRemain;
-        int value, qtNotes, qtCoins;
+        List<Integer> coins = Arrays.asList(1, 50, 25, 10, 5);
+        double toyPrice = 8.50;
+        double change;
+        int value, qtNotes, qtCoins, changeRemain;
         String result;
 
         if (money < toyPrice) {
-            throw new ArithmeticException();
+            result = "Dinheiro insuficiente";
+            return result;
         } else if (money > 100) {
             throw new IllegalArgumentException();
         } else if (money == toyPrice) {
-            throw new InputMismatchException();
+            result = "Não há troco";
+            return result;
         } else {
             change = money - toyPrice;
-            result = "Troco = R$ " + change + "\n";
+            result = "Troco = R$ " + String.format("%.2f",change) + "\n";
             value = (int) change;
 
             for (int i = 0; value != 0 && value != 1; i++) {
                 qtNotes = value / notes.get(i);
                 if (qtNotes != 0) {
-                    result = result + (qtNotes + " nota(s) de R$" + notes.get(i) + "\n");
+                    result = result + (qtNotes + " nota(s) de R$" + notes.get(i) + ",00.\n");
                     value = value % notes.get(i);
                 }
             }
             result = result + "\n";
-            changeRemain = (change - (int) change) + value;
+            if(value == 1){
+                result = result + ("1 moeda de R$" + coins.get(0) + "\n");
+            }
+            changeRemain = (int)Math.round((change - (int) change) * 100);
+            System.out.println(changeRemain);
 
-            for (int i = 0; changeRemain != 0; i++) {
-                qtCoins = (int) (changeRemain / coins.get(i));
+            for (int i = 1; changeRemain != 0 && changeRemain >= 5; i++) {
+                qtCoins = changeRemain / coins.get(i);
                 if (qtCoins != 0) {
-                    result = result + (qtCoins + " moeda(s) de R$" + coins.get(i) + "\n");
+                    result = result + (qtCoins + " moeda(s) de " + coins.get(i) + " centavos.\n");
                     changeRemain = changeRemain % coins.get(i);
                 }
             }
